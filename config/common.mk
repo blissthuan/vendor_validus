kn8wolf/vendor_slim

Change how we change the lcd density
This allows us to edit the props without re-zipping

Change-Id: I6d59ecf79f765009e57deabccd5de074b7a7d006

config/common.mk
@@ -179,3 +179,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(SLIM_MOD_VERSION) \
     ro.slim.buildtype=$(SLIM_BUILD_TYPE)
 
+EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
+

blissthuan/vendor_validus

config/common.mk
@@ -255,3 +255,5 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 -include vendor/cyngn/product.mk
 
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+
+EXTENDED_POST_PROCESS_PROPS := vendor/validus/tools/slim_process_props.py