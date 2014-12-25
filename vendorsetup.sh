632758fa264424b5e5c3d98d5bd1ae1419ed3275
Adam <ovadia.adam@gmail.com>
2014-12-25 17:20:09 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,12 +2,16 @@ add_lunch_combo validus_bacon-userdebug
 add_lunch_combo validus_d2att-userdebug
 add_lunch_combo validus_d2tmo-userdebug
 add_lunch_combo validus_d2vzw-userdebug
+add_lunch_combo validus_d850-userdebug
 add_lunch_combo validus_d851-userdebug
 add_lunch_combo validus_d852-userdebug
+add_lunch_combo validus_d855-userdebug
 add_lunch_combo validus_falcon-userdebug
 add_lunch_combo validus_hammerhead-userdebug
 add_lunch_combo validus_hercules-userdebug
 add_lunch_combo validus_ghost-userdebug
+add_lunch_combo validus_ls990-userdebug
 add_lunch_combo validus_m8-userdebug
 add_lunch_combo validus_mako-userdebug
 add_lunch_combo validus_shamu-userdebug
+add_lunch_combo validus_vs985-userdebug
