00a2d505cb469bd79f0dfd5c0a5c19f03bfa1380
John Brewer <gzroms@hotmail.com>
2014-12-31 16:02:20 -0500

    Build screen recorder

--- config/common.mk
+++ config/common.mk
@@ -198,6 +198,11 @@ PRODUCT_PACKAGES += \
     Superuser \
     su
 
+# Screen recorder
+PRODUCT_PACKAGES += \
+    ScreenRecorder \
+    libscreenrecorder
+	
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 else
