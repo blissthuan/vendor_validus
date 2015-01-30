d6da920c81736fc4deabbe33f61fa477786596c5
martin coulon <martinusbe@gmail.com>
2015-01-30 18:16:58 +0100

    Add serranoltexx, s4mini EU LTE

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,6 +37,7 @@ add_lunch_combo validus_p3110-userdebug
 add_lunch_combo validus_pollux_windy-userdebug
 add_lunch_combo validus_quincyatt-userdebug
 add_lunch_combo validus_shamu-userdebug
+add_lunch_combo validus_serranoltexx-userdebug
 add_lunch_combo validus_sirius-userdebug
 add_lunch_combo validus_togari-userdebug
 add_lunch_combo validus_vs980-userdebug
