0c4e5450b1d60f4aefe472d0638ff65b70fbc2b8
martin coulon <martinusbe@gmail.com>
2015-02-22 18:24:48 +0100

    Vendorsetup: devices by OEM
    prolly missed one or two

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
-add_lunch_combo validus_amami-userdebug
-add_lunch_combo validus_bacon-userdebug
-add_lunch_combo validus_chagallwifi-userdebug
-add_lunch_combo validus_deb-userdebug
-add_lunch_combo validus_d2att-userdebug
-add_lunch_combo validus_d2bst-userdebug
-add_lunch_combo validus_d2tmo-userdebug
-add_lunch_combo validus_d2vzw-userdebug
+## HTC
+###############
+add_lunch_combo validus_m8-userdebug
+
+## LG
+###############
 add_lunch_combo validus_d800-userdebug
 add_lunch_combo validus_d801-userdebug
 add_lunch_combo validus_d802-userdebug
@@ -16,38 +14,60 @@ add_lunch_combo validus_d852-userdebug
 add_lunch_combo validus_d855-userdebug
 add_lunch_combo validus_e970-userdebug
 add_lunch_combo validus_e973-userdebug
-add_lunch_combo validus_falcon-userdebug
-add_lunch_combo validus_flo-userdebug
 add_lunch_combo validus_f320-userdebug
 add_lunch_combo validus_geehrc-userdebug
+add_lunch_combo validus_ls980-userdebug
+add_lunch_combo validus_ls990-userdebug
+add_lunch_combo validus_vs980-userdebug
+add_lunch_combo validus_vs985-userdebug
+
+## Motorola
+###############
+add_lunch_combo validus_falcon-userdebug
 add_lunch_combo validus_ghost-userdebug
+add_lunch_combo validus_mb886-userdebug
+
+## Nexus
+###############
+add_lunch_combo validus_deb-userdebug
+add_lunch_combo validus_flo-userdebug
 add_lunch_combo validus_grouper-userdebug
 add_lunch_combo validus_hammerhead-userdebug
+add_lunch_combo validus_mako-userdebug
+add_lunch_combo validus_shamu-userdebug
+
+## OnePlus
+###############
+add_lunch_combo validus_bacon-userdebug
+
+## Samsung
+###############
+add_lunch_combo validus_chagallwifi-userdebug
+add_lunch_combo validus_d2att-userdebug
+add_lunch_combo validus_d2bst-userdebug
+add_lunch_combo validus_d2tmo-userdebug
+add_lunch_combo validus_d2vzw-userdebug
 add_lunch_combo validus_hercules-userdebug
 add_lunch_combo validus_hlte-userdebug
 add_lunch_combo validus_hltespr-userdebug
 add_lunch_combo validus_hltetmo-userdebug
-add_lunch_combo validus_honami-userdebug
 add_lunch_combo validus_i9100-userdebug
 add_lunch_combo validus_i9300-userdebug
-add_lunch_combo validus_ls980-userdebug
-add_lunch_combo validus_ls990-userdebug
 add_lunch_combo validus_lt01wifi-userdebug
-add_lunch_combo validus_m8-userdebug
-add_lunch_combo validus_mako-userdebug
 add_lunch_combo validus_matissewifi-userdebug
-add_lunch_combo validus_mb886-userdebug
 add_lunch_combo validus_n7100-userdebug
-add_lunch_combo validus_odin-userdebug
 add_lunch_combo validus_p3110-userdebug
-add_lunch_combo validus_pollux_windy-userdebug
 add_lunch_combo validus_quincyatt-userdebug
-add_lunch_combo validus_shamu-userdebug
 add_lunch_combo validus_serrano3gxx-userdebug
 add_lunch_combo validus_serranoltexx-userdebug
+
+## Sony
+###############
+add_lunch_combo validus_amami-userdebug
+add_lunch_combo validus_honami-userdebug
+add_lunch_combo validus_odin-userdebug
+add_lunch_combo validus_pollux_windy-userdebug
 add_lunch_combo validus_sirius-userdebug
 add_lunch_combo validus_togari-userdebug
-add_lunch_combo validus_vs980-userdebug
-add_lunch_combo validus_vs985-userdebug
 add_lunch_combo validus_yuga-userdebug
 add_lunch_combo validus_z3-userdebug
