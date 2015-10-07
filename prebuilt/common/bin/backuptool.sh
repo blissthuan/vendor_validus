6b39c12abb4d16f8c6f7cad6a4a72a4ab08a2642
Michael Bestas <mikeioannina@gmail.com>
2015-10-07 18:38:03 +0300

    Updates for validus M
    Change-Id: I6a0efff3bf6ea2e332cacd7cbb2f2b132b2c73dd
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	CONTRIBUTORS.mkdn
    	README.mkdn
    	config/common.mk
    	prebuilt/common/bin/50-cm.sh
    	prebuilt/common/bin/backuptool.functions
    	prebuilt/common/bin/backuptool.sh
    	vendorsetup.sh

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=LP-v8
+export V=LP-v9
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
